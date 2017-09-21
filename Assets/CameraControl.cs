1d15019202fc108351b553fd1d5a233f34b8c985
Keanan Pucci <kcp320@nyu.edu>
2017-09-21 10:57:04 -0400

    Test

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this script will move the camera and make it look at stuff
+public class CameraControl : MonoBehaviour {
+
+	public Transform lookAtTarget; //what is the camera supposed to look at?
+	public Transform moveToTarget; //where is the camera supposed to go?
+
+	// Update is called once per frame
+	void Update () {
+		//is lookAtTarget defined?
+		if (lookAtTarget != null) {
+			//make this transform look at thing
+			transform.LookAt( lookAtTarget.position );
+		}
+		//is moveToTarget defined?
+		if (moveToTarget != null) {
+			//make this transform move to thing
+
+			//calculate vector from camera to destination
+			Vector3 moveDirection = moveToTarget.position - transform.position;
+
+			//normalize movement
+			if (moveDirection.magnitude > 1f) {
+				moveDirection = moveDirection.normalized; //standardize magnitude to 1
+			}
+
+			//move this transform towards its destination
+			transform.position+=moveDirection *Time.deltaTime*10f;
+
+		}
+	}
+}
---
+++ Assets/CameraTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//put this script on trigger
+public class CameraTrigger : MonoBehaviour {
+
+	public Transform lookOverride; // what the trigger sets the main camera to look at
+	public Transform moveOverride; // what the trigger sets the main camera to move to
+
+	// Run when something enters trigger
+	void OnTriggerEnter (Collider activator) {
+		Debug.Log ("Camera move triggered!");
+
+		//to find the CameraControol component, we'll start at the Main Camera
+		CameraControl camControl = Camera.main.GetComponent<CameraControl>();
+
+		//override Camera values
+		if (lookOverride != null) {
+			camControl.lookAtTarget = lookOverride;
+		}
+		if (moveOverride != null) {
+			camControl.moveToTarget = moveOverride;
+		}
+	}
+
+	void OnDrawGizmos (){
+		if (lookOverride != null) {
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawLine (transform.position, lookOverride.position);
+		}
+		if (moveOverride != null) {
+			Gizmos.color = Color.magenta;
+			Gizmos.DrawLine (transform.position, moveOverride.position);
+		}
+	}
+
+}
